b812b62b4e9fa7cd4ff65acca1127d7218411e3e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-08-29 12:35:45 +0200

    Update module.

--- templates/path-disclaimer.tpl.php
+++ templates/path-disclaimer.tpl.php
@@ -1,1 +1,21 @@
-Please override the default disclaimer.
+<h2>Warning</h2>
+
+<p>There is no disclaimer available!</p>
+
+<p>Don't worry, you can create one by creating a file <code>path-disclaimer.tpl.php</code> in your current theme.</p>
+
+<h2>Customizations</h2>
+
+<p>You can customize the disclaimer per url and path by creating different template names.</p>
+
+<p>Example for the page <code>node/1</code></p>
+
+<ul>
+  <li>path-disclaimer--node-1.tpl.php</li>
+</ul>
+
+<p>Or if you want to customize the disclaimer per url and per language</p>
+
+<ul>
+  <li>path-disclaimer--node-1--fr.tpl.php</li>
+</ul>
